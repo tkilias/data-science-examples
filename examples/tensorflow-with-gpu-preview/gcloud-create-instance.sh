ce037edd6d32773e9714727aa124700f0ec62506
Torsten Kilias <torsten.kilias@exasol.com>
2020-01-07 13:13:42 +0100

    increate cpus

--- examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
+++ examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
@@ -2,7 +2,7 @@ NAME=$1
 shift
 gcloud compute instances create $NAME \
 --custom-memory=156GB  \
---custom-cpu=16  \
+--custom-cpu=24  \
 --boot-disk-auto-delete  \
 --boot-disk-size=200GB  \
 --image=projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20191211  \
