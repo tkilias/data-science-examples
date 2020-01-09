b537f625d8cfd2fe24dbcfc3e0f3da85554450ec
Torsten Kilias <torsten.kilias@exasol.com>
2020-01-09 10:40:38 +0100

    Setup RAM Size for V100 GPU

--- examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
+++ examples/tensorflow-with-gpu-preview/gcloud-create-instance.sh
@@ -1,8 +1,8 @@
 NAME=$1
 shift
 gcloud compute instances create $NAME \
---custom-memory=156GB  \
---custom-cpu=24  \
+--custom-memory=78GB  \
+--custom-cpu=12  \
 --boot-disk-auto-delete  \
 --boot-disk-size=200GB  \
 --image=projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20191211  \
